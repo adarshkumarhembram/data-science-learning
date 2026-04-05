5625821b5575e93df662eebbdd71fc19b4582167
adarshkumarhembram <hembramadarshkumar@gmail.com>
2026-04-05 18:35:22 +0530

    oops basics

---
+++ 01_python/10_Oops.py
@@ -0,0 +1,24 @@
+class Car:
+    def __init__(self,brand,model):
+        self.brand = brand
+        self.model = model
+    
+    def full_name(self):
+        return f"{self.brand} {self.model}"
+
+class ElectricCar(Car):
+    def __init__(self,brand,model,battery_size):
+        super().__init__(brand, model)
+        self.battery_size = battery_size  
+
+my_tesla = ElectricCar("Tesla", "Model S", "85kWh")
+print(my_tesla.full_name())
+
+
+# my_car = Car("Tyotta","Corolla")
+# print(my_car.brand)
+# print(my_car.model)
+# print(my_car.full_name())
+
+# my_newCar = Car("Tata","Safari")
+# print(my_newCar.model)
